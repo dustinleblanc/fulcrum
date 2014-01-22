55a96d8de1fc9037e6c9008fda141424346c324c
dustinleblanc <dustin@singlebrook.com>
2014-01-22 10:00:16 -0500

    fixes unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-worker_processes 3
-timeout 30
-preload_app true
-
-before_fork do |server, worker|
-
-=======
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
 before_fork do |server, worker|
->>>>>>> production
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -22,14 +13,8 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-<<<<<<< HEAD
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-=======
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
->>>>>>> production
   end
 
   defined?(ActiveRecord::Base) and
